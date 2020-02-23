0e12b7d6c2d5d60c037028bcceffc8e3bb6bf667
cstrahl <christiandstrahl@gmail.com>
2020-02-23 18:38:54 -0500

    Initial Commit

---
+++ tqa/src/main/java/com/group2/tqa/TqaApplication.java
@@ -0,0 +1,13 @@
+package com.group2.tqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TqaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TqaApplication.class, args);
+	}
+
+}
