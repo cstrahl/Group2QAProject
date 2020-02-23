52d164e97de7e78c0de8352533262429af49c549
cstrahl <christiandstrahl@gmail.com>
2020-02-23 18:38:54 -0500

    Initial Commit

---
+++ tqa/src/test/java/com/group2/tqa/TqaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group2.tqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TqaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
